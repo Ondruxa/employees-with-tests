c8143b00d65919c18ec007b25a1edcaa590374a6
Andrei Gorshkov <andruhinsans@mail.ru>
2024-12-01 14:11:56 +0300

    first commit

---
+++ src/main/java/com/example/employees_with_tests/EmployeesWithTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.employees_with_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesWithTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesWithTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employees_with_tests/EmloyeesWithTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employees_with_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmloyeesWithTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
